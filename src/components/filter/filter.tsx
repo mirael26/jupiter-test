d120fb5169005053db45e84d297df9adf01fdbc4
Elmira Ilyasova <capricorn_26@mail.ru>
2022-06-22 23:50:15 +0700

    Add mobile version

--- src/components/filter/filter.tsx
+++ src/components/filter/filter.tsx
@@ -1,28 +1,59 @@
 import * as React from "react";
+import { useState } from "react";
 
 const Filter = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <ul className="filter">
-      <li className="filter__item">
-        <button className="filter__button active" disabled={true}>Show all</button>
-      </li>
+    <React.Fragment>
+      <div className="filter filter--desktop">
+        <ul className="filter__list">
+          <li className="filter__item">
+            <button className="filter__button active" disabled={true}>Show all</button>
+          </li>
 
-      <li className="filter__item">
-        <button className="filter__button">Design</button>
-      </li>
+          <li className="filter__item">
+            <button className="filter__button">Design</button>
+          </li>
 
-      <li className="filter__item">
-        <button className="filter__button">Branding</button>
-      </li>
-        
-      <li className="filter__item">
-        <button className="filter__button">Illustration</button>
-      </li>
+          <li className="filter__item">
+            <button className="filter__button">Branding</button>
+          </li>
+            
+          <li className="filter__item">
+            <button className="filter__button">Illustration</button>
+          </li>
 
-      <li className="filter__item">
-        <button className="filter__button">Motion</button>
-      </li>
-    </ul>
+          <li className="filter__item">
+            <button className="filter__button">Motion</button>
+          </li>
+        </ul>
+      </div>
+
+      <div className="filter filter--mobile">
+        <button className="filter__open-button" onClick={() => setOpen(!isOpen)}>ShowAll</button>
+
+        {isOpen
+          ? <ul className="filter__list">
+              <li className="filter__item">
+                <button className="filter__button">Design</button>
+              </li>
+
+              <li className="filter__item">
+                <button className="filter__button">Branding</button>
+              </li>
+                
+              <li className="filter__item">
+                <button className="filter__button">Illustration</button>
+              </li>
+
+              <li className="filter__item">
+                <button className="filter__button">Motion</button>
+              </li>
+            </ul>
+          : null}
+      </div>
+    </React.Fragment>
   );
 };
 
