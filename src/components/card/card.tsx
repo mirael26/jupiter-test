49a852a91998d71c3afc3cdf0a5633d5478f6861
Elmira Ilyasova <capricorn_26@mail.ru>
2022-06-24 00:09:57 +0700

    Add logic to cards list

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
 
-const Card = () => {
+import { Card } from "../../types";
+
+interface CardProps {
+  data: Card,
+}
+
+const Card = ({data}: CardProps): JSX.Element => {
+
   return (
-    <div className="card"
-      style={{backgroundImage: `linear-gradient(rgba(92, 32, 0, 0.32), rgba(92, 32, 0, 0.32)), url(${require("./../../img/cards/sofa.png")})`}}>
-      <div className="card__category">Design</div>
-      <h2 className="card__title">Sofa</h2>   
+    <div className="card" id={data.id}
+      style={{backgroundImage: `linear-gradient(rgba(92, 32, 0, 0.32), rgba(92, 32, 0, 0.32)), url(${require(`./../../img/cards/${data.image}`)})`}}>
+      <button className="card__category">{data.category}</button>
+      <h2 className="card__title">{data.title}</h2>   
     </div>
   );
 };
--- src/components/portfolio/portfolio.tsx
+++ src/components/portfolio/portfolio.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import { cardMocks } from "../../mocks";
 import { Card as CardType } from "../../types";
@@ -8,28 +8,97 @@ import Container from "../container/container";
 import Filter from "../filter/filter";
 import Card from "../card/card";
 
+const AMOUNT_STEP = 9;
+const CARD_CLASS = "card";
+const CARD_CATEGORY_CLASS = "card__category";
+
+let activeCardId: string = null;
+
 const Portfolio = (): JSX.Element => {
   const [filter, setFilter] = useState("Show All")
-  const [displayedCards, setDisplayedCards] = useState(cardMocks);
+  const [cards, setCards] = useState(cardMocks);
+  const [cardsAmount, setCardsAmount] = useState(AMOUNT_STEP);
+  
+  useEffect (
+    () => {
+      document.addEventListener('keydown', onDeleteKeydown);
+      return () => {
+        document.removeEventListener('keydown', onDeleteKeydown);
+      };
+    }, [cards]
+  )
 
-  const filters = Array.from(new Set(displayedCards.map((card: CardType) => card.category)));
+  const onDeleteKeydown = (evt: KeyboardEvent) => {
+    if (evt.isComposing || evt.key === "Delete") {
+      evt.preventDefault();
+      setCards(cards.filter(card => card.id !== activeCardId));
+      activeCardId = null;
+      document.querySelector(".card.active").classList.remove("active");
+    }
+  }
+
+  const loadMoreCards = () => {
+    setCardsAmount(cardsAmount + AMOUNT_STEP);
+  };
+
+  const onCardsClick = (evt: React.SyntheticEvent) => {    
+    const element = evt.target as Element;
+
+    if (element.classList.contains(CARD_CATEGORY_CLASS)) {
+      setFilter(element.textContent);
+      return;
+    }
+
+    if (element.classList.contains(CARD_CLASS)) {
+      if (element.classList.contains("active")) {
+        element.classList.remove("active");
+        activeCardId = null;
+        return;
+      }
+      
+      activeCardId !== null ? document.querySelector(".card.active").classList.remove("active") : null;
+      element.classList.add("active");        
+      activeCardId = element.id;
+      return;
+    }
+
+    const parentElement = element.parentNode as Element;
+    if (parentElement.classList.contains(CARD_CLASS)) {
+
+      if (parentElement.classList.contains("active")) {
+
+        parentElement.classList.remove("active");
+        activeCardId = null;
+        return;
+      }
+      activeCardId !== null ? document.querySelector(".card.active").classList.remove("active") : null;
+      parentElement.classList.add("active");
+      activeCardId = parentElement.id;
+    }
+  };
+
+  const filters = Array.from(new Set(cards.map((card: CardType) => card.category)));
   filters.unshift('Show All');
 
+  const filteredCards = filter === "Show All" ? cards : cards.filter(card => card.category === filter);
+  const displayedCards = filteredCards.slice(0, cardsAmount);
+  
   return (
     <div className="portfolio">
       <Container>
         <Filter filters={filters} currentFilter={filter} changeFilter={setFilter} />
 
-        <div className="portfolio__cards">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="portfolio__cards" onClick={(evt) => onCardsClick(evt)}>
+          {displayedCards.map((card, i) => {
+            return <Card key={`${i}`} data={card}/>
+          })}
         </div>
 
-        <button className="portfolio__loadmore-button">Load more</button>
+        {filteredCards.length > cardsAmount
+          ? <button className="portfolio__loadmore-button" onClick={loadMoreCards}>Load more</button>
+          : null
+        }
+        
       </Container>
     </div>
   );
